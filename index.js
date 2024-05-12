190052fad001f147cd0e151b1341378438b6069f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-12 09:41:52 +0530

    backend code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
 const port = 8080;
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
