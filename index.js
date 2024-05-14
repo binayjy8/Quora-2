d79bda0430e949a5a6697c8c03bd8b1bb26d0a42
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-14 15:37:10 +0530

    backend code

--- index.js
+++ index.js
@@ -62,6 +62,12 @@ app.get("/posts/:id", (req, res) => {
     res.render("show.ejs",{ post });
 });
 
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.find((p) => id !== p.id);
+    res.redirect("/posts");
+})
+
 app.listen(port, (req, res) => {
     console.log(`listening to the port${port}`);
 });
