456bac5889538c48c63cd435639c10ea63e3499f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-05-14 15:22:24 +0530

    backend code

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require("path");
 const port = 8080;
 const { v4: uuidv4 } = require("uuid");
-
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
